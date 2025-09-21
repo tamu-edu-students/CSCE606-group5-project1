b6ea2e4d287585fe989696a0d92b0b7c837b6ccb
shreyasahni <shreyasahni7@gmail.com>
2025-09-21 12:58:11 -0500

    Fixed tests

--- test/controllers/calendar_controller_test.rb
+++ test/controllers/calendar_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class CalendarControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get calendar_show_url
+    get calendar_url
     assert_response :success
   end
 end
