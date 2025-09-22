0a6a3479be7682d93da8f894becc00e4fc427fbe
htumu <127043382+htumu@users.noreply.github.com>
2025-09-22 12:09:04 -0500

    skipped user and calender tests

--- test/controllers/calendar_controller_test.rb
+++ test/controllers/calendar_controller_test.rb
@@ -1,13 +1,14 @@
-# require "test_helper"
+require "test_helper"
 
-# class CalendarControllerTest < ActionDispatch::IntegrationTest
-#   test "should get show" do
-#     open_session do |sess|
-#       sess.get calendar_url
-#       sess.request.session[:google_token] = "fake_token"
-#       sess.request.session[:google_refresh_token] = "fake_refresh"
-#       sess.get calendar_url
-#       assert_includes [ 200, 302 ], sess.response.status
-#     end
-#   end
-# end
+class CalendarControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    skip("Calender UI is disabled")
+    open_session do |sess|
+      sess.get calendar_url
+      sess.request.session[:google_token] = "fake_token"
+      sess.request.session[:google_refresh_token] = "fake_refresh"
+      sess.get calendar_url
+      assert_includes [ 200, 302 ], sess.response.status
+    end
+  end
+end
