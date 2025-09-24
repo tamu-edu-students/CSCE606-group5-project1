3a5411eba6307d3aebf7e9e4583c0ade3c052912
htumu <127043382+htumu@users.noreply.github.com>
2025-09-24 17:36:55 -0500

    enabled cross site redirects in production only

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 Rails.application.config.session_store :cookie_store,
-  key: '_csce606_group5_project1_session',
-  secure: Rails.env.production?,  # ensures cookies are marked secure in production
-  same_site: :none                # allows cookies to be sent on cross-site redirects
+  key: "_csce606_group5_project1_session",
+  secure: Rails.env.production?, # ensures cookies are marked secure in production
+  same_site: Rails.env.production? ? :none : :lax # allows cookies to be sent on cross-site redirects in production
