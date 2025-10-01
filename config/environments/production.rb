021b68100af61d78ab9c8103dfaea7575ad1dd91
yafeili945 <yafeili@tamu.edu>
2025-10-01 18:52:39 -0500

    Fix Rubocop warnings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   config.assume_ssl = true
   config.force_ssl  = true
 
-  config.log_tags = [:request_id]
+  config.log_tags = [ :request_id ]
   config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
   config.silence_healthcheck_path = "/up"
@@ -49,5 +49,5 @@ Rails.application.configure do
 
   config.i18n.fallbacks = true
   config.active_record.dump_schema_after_migration = false
-  config.active_record.attributes_for_inspect = [:id]
+  config.active_record.attributes_for_inspect = [ :id ]
 end
