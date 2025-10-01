09183a2bddd315906196d2807e94e0bdaad5f014
yafeili945 <yafeili@tamu.edu>
2025-10-01 16:25:48 -0500

    Change schema to fix merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[8.0].define(version: 2025_09_26_230303) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_28_013946) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,7 +21,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_26_230303) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index [ "user_id" ], name: "index_events_on_user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "leet_code_entries", force: :cascade do |t|
@@ -32,8 +31,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_26_230303) do
     t.date "solved_on", default: -> { "CURRENT_DATE" }, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index [ "problem_number" ], name: "index_leet_code_entries_on_problem_number"
-    t.index [ "solved_on" ], name: "index_leet_code_entries_on_solved_on"
+    t.index ["problem_number"], name: "index_leet_code_entries_on_problem_number"
+    t.index ["solved_on"], name: "index_leet_code_entries_on_solved_on"
   end
 
   create_table "leet_code_problems", force: :cascade do |t|
@@ -44,7 +43,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_26_230303) do
     t.text "tags"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index [ "leetcode_id" ], name: "index_leet_code_problems_on_leetcode_id", unique: true
+    t.index ["leetcode_id"], name: "index_leet_code_problems_on_leetcode_id", unique: true
   end
 
   create_table "leet_code_session_problems", force: :cascade do |t|
@@ -54,8 +53,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_26_230303) do
     t.datetime "solved_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index [ "leet_code_problem_id" ], name: "index_leet_code_session_problems_on_leet_code_problem_id"
-    t.index [ "leet_code_session_id" ], name: "index_leet_code_session_problems_on_leet_code_session_id"
+    t.index ["leet_code_problem_id"], name: "index_leet_code_session_problems_on_leet_code_problem_id"
+    t.index ["leet_code_session_id"], name: "index_leet_code_session_problems_on_leet_code_session_id"
   end
 
   create_table "leet_code_sessions", force: :cascade do |t|
@@ -65,7 +64,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_26_230303) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index [ "user_id" ], name: "index_leet_code_sessions_on_user_id"
+    t.index ["user_id"], name: "index_leet_code_sessions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
