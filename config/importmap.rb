59ea224178e32ac9d660c62e8f71492f97bbaeed
shreyasahni <shreyasahni7@gmail.com>
2025-10-01 12:34:10 -0500

    Fixed dashboard to work w/ and w/o JS

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,11 @@
+# config/importmap.rb
+
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+
+# THIS IS THE CRITICAL LINE THAT IS LIKELY MISSING OR WRONG
+pin_all_from "app/javascript/controllers", under: "controllers"
